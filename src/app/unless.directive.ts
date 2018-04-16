89075e0912ea598a5a5ef429fbcfd1454a8d0ed4
man45045150 <man45045150@hotmail.co.th>
2018-04-16 21:29:55 +0700

    Create Structural Directive

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -4,14 +4,19 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
   selector: '[appUnless]'
 })
 export class UnlessDirective {
+  //set is keyword for excute by condition
+  //naming property same selector
   @Input() set appUnless(condition: boolean) {
     if (!condition) {
+      //create view in viewContainer
       this.vcRef.createEmbeddedView(this.templateRef);
     } else {
+      //remove everything from this place in the DOM
       this.vcRef.clear();
     }
   }
 
+  //TemplateRef is what , ViewContainerRef is where
   constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
 
 }
